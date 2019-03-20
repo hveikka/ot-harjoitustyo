a1708289a1673660f96da4c8392206eae22228e5
Veikka Hirvonen <veikkable@gmail.com>
2019-03-20 15:27:57 +0200

    committii vaha

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -15,6 +15,50 @@ public class MaksukorttiTest {
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
+    }
+
+    @Test
+    public void saldoAlussaOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+
+    @Test
+    public void saldoLisayksenJalkeenOikein() {
+        kortti.lataaRahaa(10);
+        assertEquals("saldo: 0.20", kortti.toString());
+    }
+
+    @Test
+    public void Otetaan() {
+        kortti.otaRahaa(5);
+        assertEquals("saldo: 0.5", kortti.toString());
+    }
+
+    @Test
+    public void eiOtetaTyhjasta() {
+        kortti.otaRahaa(5);
+        kortti.otaRahaa(10);
+        assertEquals("saldo: 0.5", kortti.toString());
+    }
+
+    @Test
+    public void trueJosRahaa() {
+        kortti.otaRahaa(5);
+ 
+        assertTrue(kortti.otaRahaa(3));
+    }
+
+    @Test
+    public void falseJosEiRahaa() {
+        kortti.otaRahaa(5);
+        
+        assertFalse(kortti.otaRahaa(10));
+    }
+    @Test
+    public void korttiPrinttaaOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+        
+        
     }
 }
